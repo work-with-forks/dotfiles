7b3c9bddc99c2284604fdd1b1a129fcfc5d18361
dp12 <deep.paren.12@gmail.com>
2019-01-31 14:53:40 -0500

    Change update script to use git_save_pull_and_restore

--- update.sh
+++ update.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+# Same as gspr, defined in case the former has not been sourced
+git_save_pull_and_restore() {
+    if ! git diff-index --quiet HEAD --; then
+        echo "git stash save; git pull --rebase; git stash pop"
+        git stash save; git pull --rebase; git stash pop
+    else
+        echo "git pull --rebase"
+        git pull --rebase
+    fi
+}
+
 echo "Updating .spacemacs.d"
-cd ~/.spacemacs.d && git stash save && git pull --rebase && git stash pop
+cd ~/.spacemacs.d && git_save_pull_and_restore
 
 echo "Updating .emacs.d"
-cd ~/.emacs.d && git stash save && git pull --rebase && git stash pop
+cd ~/.emacs.d && git_save_pull_and_restore
 
 echo "Updating dotfiles"
-cd ~/dotfiles && git stash save && git pull --rebase && git stash pop
+cd ~/dotfiles && git_save_pull_and_restore
 
