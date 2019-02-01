88e71d429f7beacd82ad37260bea7d7ec0a67af3
dp12 <deep.paren.12@gmail.com>
2019-02-01 15:54:02 -0500

    Use cowsay for update

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@
 # Same as gspr, defined in case the former has not been sourced
 git_save_pull_and_restore() {
     if ! git diff-index --quiet HEAD --; then
-        echo "git stash save; git pull --rebase; git stash pop"
+        cowsay "git stash save; git pull --rebase; git stash pop"
         git stash save; git pull --rebase; git stash pop
     else
-        echo "git pull --rebase"
+        cowsay "git pull --rebase"
         git pull --rebase
     fi
 }
